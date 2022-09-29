4a2137a7550e9871b10080e8c9b14ddafd7aa35a
Massimo Ercolani <massimo.ercolani28@gmail.com>
2022-09-29 15:17:28 +0200

    fix README

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,3 +1,5 @@
+import Head from "next/head"
+
 type LayoutProps = {
   children: React.ReactNode
 }
@@ -5,6 +7,9 @@ type LayoutProps = {
 export const Layout = ({ children }: LayoutProps) => {
   return (
     <div className="min-w-screen min-h-screen flex justify-center font-sans overflow-hidden">
+      <Head>
+        <title>Parloa CRM</title>
+      </Head>
       <div className="px-2 sm:px-6 w-full lg:w-5/6">
         {children}
       </div>
